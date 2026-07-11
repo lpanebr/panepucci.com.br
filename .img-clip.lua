129ec9c612eec48be673a0fe60c6913e741d669d
Luciano Gabriel Panepucci <lpanebr@gmail.com.br>
2026-07-11 14:20:00 -0300

    img-clip.nvim project config.

---
+++ .img-clip.lua
@@ -0,0 +1,37 @@
+local function current_file_path()
+  return vim.fn.expand("%:p")
+end
+
+local function is_sinapse_markdown()
+  return current_file_path():match("/src/content/sinapses/[^/]+%.md$") ~= nil
+end
+
+local function current_slug()
+  return vim.fn.expand("%:t:r")
+end
+
+local function project_root()
+  return current_file_path():match("^(.-)/src/content/sinapses/[^/]+%.md$")
+end
+
+return {
+  custom = {
+    {
+      trigger = is_sinapse_markdown,
+      dir_path = function()
+        local root = project_root()
+        local slug = current_slug()
+
+        if not root or not slug then
+          return "public/sinapses"
+        end
+
+        return root .. "/public/sinapses/" .. slug
+      end,
+      relative_template_path = false,
+      template = function(context)
+        return string.format("![%s](/sinapses/%s/%s)", context.cursor, current_slug(), context.file_name)
+      end,
+    },
+  },
+}
